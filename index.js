7e993587da95edd55434e0324773c7b885428e26
Anthony Beltran <beltrananthony30@icloud.com>
2023-07-25 23:18:03 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+const yourName = "Anthony Beltran"; 
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
